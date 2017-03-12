8be8cc1cdd2d2a6ab55b6b72066200df75003d43
Felix Krause <krausefx@gmail.com>
2017-03-12 15:56:55 -0700

    Update parser to support before_all, after_all, error blocks and move printing into its own class

--- parser.rb
+++ parser.rb
@@ -1,68 +1,99 @@
 require 'pry'
 require 'json'
 
-class Parser
-  attr_accessor :path
-  attr_accessor :content
-  attr_accessor :tree
+# Parse a Fastfile and convert it to JSON data
+module Fastlane
+  class Parser
+    attr_accessor :path
+    attr_accessor :content
+    attr_accessor :tree
 
-  def load(path: nil)
-    self.path = path
-    self.content = File.read(path)
-    self.tree = {}
+    def initialize(path: nil)
+      self.path = path
+      self.content = File.read(path)
+      self.tree = {}
 
-    eval(self.content, parsing_binding)
+      eval(self.content, parsing_binding)
+    end
 
-    puts "-------"
-    puts JSON.pretty_generate(self.tree)
-    puts "-------"
-  end
+    def parsing_binding
+      binding
+    end
 
-  def parsing_binding
-    binding
-  end
+    def print
+      STDOUT.puts "-------"
+      STDOUT.puts JSON.pretty_generate(self.tree)
+      STDOUT.puts "-------"
+    end
 
+    #####################################################
+    # @!group DSL
+    #####################################################
 
-  #####################################################
-  # @!group DSL
-  #####################################################
+    def platform(platform_name, &block)
+      @current_platform = platform_name
+      block.call
+      @current_platform = nil
+    end
 
-  def platform(platform_name, &block)
-    @current_platform = platform_name
-    block.call
-    @current_platform = nil
-  end
+    def lane(lane_name, &block)
+      description = @current_description
+      @current_lane = lane_name
 
-  def lane(lane_name, &block)
-    description = @current_description
+      self.tree[@current_platform] ||= {}
+      self.tree[@current_platform][@current_lane] = {
+        description: description,
+        actions: []
+      }
 
-    self.tree[@current_platform] ||= {}
-    self.tree[@current_platform][lane_name] = {
-      description: description,
-      actions: []
-    }
+      block.call
 
-    block.call
+      @current_description = []
+      @current_lane = nil
+    end
 
-    @current_description = []
-  end
+    def desc(str)
+      @current_description ||= []
+      @current_description << str
+    end
 
-  def desc(str)
-    @current_description ||= []
-    @current_description << str
-  end
+    def before_all
+      @current_lane = :_before_all_block_
+      yield
+      @current_lane = nil
+    end
 
-  def method_missing(method_sym, *arguments, &_block)
-    current_lane = self.tree[@current_platform].values.last
-    current_lane[:actions] << {
-      action: method_sym,
-      parameters: arguments.first
-    }
+    def after_all
+      @current_lane = :_after_all_block_
+      yield
+      @current_lane = nil
+    end
+
+    def error
+      @current_lane = :_error_block_
+      yield
+      @current_lane = nil
+    end
+
+    def method_missing(method_sym, *arguments, &_block)
+      self.tree[@current_platform] ||= {}
+      current_lane = self.tree[@current_platform][@current_lane] ||= {}
+      current_lane[:actions] ||= []
+      current_lane[:actions] << {
+        action: method_sym,
+        parameters: arguments.first || {}
+      }
+    end
+
+    def puts(str)
+      method_missing(:puts, str)
+    end
+
+    def say(value)
+      method_missing(:say, str)
+    end
   end
 end
-
-Parser.new.load(path: "./examples/Fastfile1")
-# Parser.new.load(path: "./examples/Fastfile2")
 
 # Target Format
 # {
